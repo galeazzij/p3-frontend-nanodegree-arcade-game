e29136a30d24e0c660edc2a1f20a3eb6f85c0296
galeazzij <galeazzij@users.noreply.github.com>
2015-12-13 14:12:32 -0500

    Added Player class and finished Enemy class

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y =y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,6 +26,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+};
 
 
 // Now instantiate your objects.
