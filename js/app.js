ded1c6f6c610962c2bb1f76c0b81c75d206d9615
galeazzij <galeazzij@users.noreply.github.com>
2015-12-13 14:55:12 -0500

    Added player object

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ var Player = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = {};
 
 
 
