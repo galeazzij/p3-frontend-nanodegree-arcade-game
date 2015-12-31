953720c75a0165abfd8d85f90de950dabfc3f785
galeazzij <galeazzij@users.noreply.github.com>
2015-12-30 20:43:50 -0500

    Modified the code in engine.js on line 48 so that the appropriate parameters were passed.

--- js/engine.js
+++ js/engine.js
@@ -45,7 +45,7 @@ var Engine = (function(global) {
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
+        update(dt, allEnemies, player);
         render();
 
         /* Set our lastTime variable which is used to determine the time delta
